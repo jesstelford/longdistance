cf5ab3a92f7fac38812261774b5c46d1eece0056
Jess Telford <jess@jes.st>
2013-07-10 13:00:40 +1000

    Add a module boilerplate file

---
+++ app/module-boilerplate.js
@@ -0,0 +1,14 @@
+define([
+       'jquery',
+       'underscore',
+       'backbone',
+], function ($, _, Backbone) {
+
+	var Module = {
+		initialize: function() {
+			// ...
+		}
+	};
+
+	return Module;
+});
