cd3e4f2a127d2ab0c371507246bbbef7933ae485
Jess Telford <jess@jes.st>
2013-07-10 16:56:37 +1000

    Better module boilerplate code

--- app/module-boilerplate.js
+++ app/module-boilerplate.js
@@ -1,14 +1,23 @@
-define([
+define(
+    // Module's name for dependancy lookups
+    "ModuleName",
+    [
+        // depends on these modules
        'jquery',
        'underscore',
        'backbone',
-], function ($, _, Backbone) {
+    ],
+// each dependancy is passed in as a parameter
+function ($, _, Backbone) {
 
-	var Module = {
-		initialize: function() {
-			// ...
-		}
-	};
+    // The module we're creating
+    var Module = {
+        // with publicly scoped members
+        initialize: function() {
+            // ...
+        }
+    };
 
-	return Module;
+    // return the module
+    return Module;
 });
