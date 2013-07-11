aa9d15b59bcf1f33904dc97a794f548fca763763
Jess Telford <jess@jes.st>
2013-07-11 10:04:39 +1000

    Latest boilerplate module code

--- app/boilerplateModule.js
+++ app/boilerplateModule.js
@@ -1,14 +1,12 @@
-define(
-    // Module's name for dependancy lookups
-    "ModuleName",
-    [
-        // depends on these modules
-       'jquery',
-       'underscore',
-       'backbone',
-    ],
-// each dependancy is passed in as a parameter
-function ($, _, Backbone) {
+// Require this module by referencing its path directly in the define
+// dependencies array
+define([
+    // depends on these modules
+    // each dependancy is passed in as a parameter
+    'jquery',
+    'underscore',
+    'backbone',
+], function ($, _, Backbone) {
 
     // The module we're creating
     var Module = {
