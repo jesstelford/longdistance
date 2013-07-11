c03a1ec6d617b876dea54c058a4844b39c2849c6
Jess Telford <jess@jes.st>
2013-07-11 11:28:48 +1000

    Usage notes for the boilerplate module

--- app/modules/boilerplate/module.js
+++ app/modules/boilerplate/module.js
@@ -7,10 +7,31 @@ define([
     var Module = App.module.extend({
 
         initialize: function() {
-            this.Router = new Router('[URL-PREFIX]')
+            this.Router = new Router('[URL-PREFIX]', {createTrailingSlashRoutes: true});
         }
     });
 
     // return the module
     return Module;
 });
+
+/** Usage **/
+
+/* app/router.js */
+/*
+    routes: {
+        // ...
+        // The "/sub" will match any subroutes
+        "my-module(/*sub)": "myModule",
+        // ...
+    },
+
+    myModule: function() {
+        // Lazily load the module
+        if (!App.Modules.Boilerplate) {
+            App.Modules.Boilerplate = new BoilerplateModule();
+        }
+    },
+
+    // ...
+*/
